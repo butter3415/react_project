95d381b943eb2e7c6668341b22b4f17f96f2c06b
butter3415 <butter3415@gmail.com>
2025-05-10 10:58:49 +0900

    250510

---
+++ src/components/MainRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './HomePage'
+import CartPage from './CartPage'
+import LoginPage from './LoginPage'
+
+const MainRouter = () => {
+    return (
+        <Container>
+            <Routes>
+                <Route>
+                    <Route path='/' element={<HomePage/>}/>
+                    <Route path='/cart' element={<CartPage/>}/>
+                    <Route path='/login' element={<LoginPage/>}/>
+                </Route>
+            </Routes>
+        </Container>
+    )
+}
+
+export default MainRouter
